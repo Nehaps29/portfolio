eb3ae87fded14c5ccf1ad539e169872695238553
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-11-18 15:21:21 -0500

    added style to resume page

--- portfolio/src/pages/Resume.jsx
+++ portfolio/src/pages/Resume.jsx
@@ -1,36 +1,37 @@
 import React from 'react';
 import { Typography, Link, List, ListItem, ListItemText } from '@mui/material';
 import Layout from '../components/Layout';
-export default function Resume() {
+
+const Resume = () => {
   return (
     <Layout>
-    <div>
-      <Typography variant="h1">Developer's Resume</Typography>
-      
-      <Typography variant="body1">Download the resume:</Typography>
-      <Link href="./src/assets/resume.pdf" download="resume.pdf">
-        Download Resume
-      </Link>
-
-      <Typography variant="h2">Proficiencies</Typography>
-      <List>
-        <ListItem>
-          <ListItemText primary="React.js" />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="JavaScript" />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="HTML" />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="CSS" />
-        </ListItem>
-        {/* Add more proficiencies as needed */}
-      </List>
-    </div>
+      <div>
+        <Typography variant="h5">Developer's Resume</Typography>
+        <Link href="./src/assets/resume.pdf" download="resume.pdf">
+          Download Resume
+        </Link>
+        <div style={{ marginBottom: '20px' }} />
+        <Typography variant="h5">Proficiencies</Typography>
+        <List dense>
+          <ListItem>
+            <ListItemText primary="React.js" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="JavaScript" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="HTML" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="CSS" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Mongoose" />
+          </ListItem>
+        </List>
+      </div>
     </Layout>
   );
 };
 
-
+export default Resume;
